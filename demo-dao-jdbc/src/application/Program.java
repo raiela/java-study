f00ff7c46dda6ea17a934898809f1452ea7fc5d3
Raiela Quirnio <raielaquirino@gmail.com>
2024-08-12 14:54:27 -0300

    Seller Entity class

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
 import models.entities.Department;
+import models.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 30000.0, obj);
+        System.out.println(seller);
     }
 }
